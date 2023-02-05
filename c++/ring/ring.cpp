9b5e5cdda4511aad18bd8f319fdd0d6e78d7a5c8
darkquesh <erenkaradag0001@hotmail.com>
2023-02-05 22:37:20 +0100

    Significant overhaul, data encapsulation fixed

--- c++/ring/ring.cpp
+++ c++/ring/ring.cpp
@@ -1,161 +1,94 @@
-/* Circular doubly-linked list */
 /* the source file ring.cpp */
+// 02/2023
 
 #include "ring.hpp"
 
-template<typename Key, typename Info>
-Ring<Key, Info>::~Ring() {          // Destructor for class Ring
-    Node* tmp = start;
-    Node* tail = new Node();
-
-    do {
-        tail = tmp;
-        tmp = tmp->next;
-    } while (tmp->next != start);
-
-    tail = tail->next;
-
-    if (start) {                    // is ring empty?
-        Node* temp = tail;
-
-        while (temp != start) {     // delete nodes until reaching the start node
-            temp->next = start->next;
-            delete start;
-            start = temp->next;
-        }
-        if (temp == start) {        // one node left
-            delete start;
-            start = nullptr;
-        }
-    }
-}
-
-
-template<typename Key, typename Info>
-void Ring<Key, Info>::remove(Key key, Info info) {  // Node removal by key and info
-    if (!start) {
-        cout << "Cannot remove, the ring is empty!" << endl;
-        return;
-    }
-    Node* tmp = start;
-
-    if (tmp->prev == tmp->next) {      // There is only one node in the ring
-        start = nullptr;
-        delete tmp;
-        return;
-    }
-
-    if (tmp->key == key && tmp->info == info) {     // The ring has more than 1 node and
-        Node* toDel = tmp;                          // the node to be removed is the first node
-        tmp = start->prev;
-        start = start->next;
-        tmp->next = start;
-        start->prev = tmp;
-        delete toDel;
-        return;
-    }
-
-    Node* last = tmp;
-
-    while ((tmp->key != key) || (tmp->info != info)) {
-        if (tmp->next == start) {
-            cout << "**Cannot remove, the specified key and info do not exist!**" << endl;
-            return;
-        }
-        last = tmp;
-        tmp = tmp->next;
-    }
-
-    if (tmp->next == start) {       // If it is the last node
-        Node* toDel = tmp;
-        last->next = start;
-        start->prev = last;
-        delete toDel;
-    }
-
-    else {
-        Node* toDel = tmp;
-        tmp->next->prev = last;
-        last->next = tmp->next;
-        delete toDel;
-    }
-}
-
-// For instance split_by_pos
-//
-// ring={1,}{2,}{3,}{4,}{5,}
-// start_pos=1, direct=true, repetitions=4,
-// direct1=true, len1=2,
-// direct2=false, len2=1
-//
-// ring1={2,}{3,}{5,}{1,}{3,}{4,}{1,}{2,}
-// ring2={3,}{5,}{2,}{4,}
-
-template<typename Key, typename Info>
-void split_by_pos (const Ring <Key, Info>& ring, int start_pos, bool direct, int repetitions, 
-Ring <Key, Info>& ring1, bool direct1, int len1, 
-Ring <Key, Info>& ring2, bool direct2, int len2)
-{   
-    if (start_pos < 1) {
-        cout << "split_by_key failed, enter a valid start_pos value!" << endl;
-        return;
-    }
-
-    auto itr = ring.begin();
-    auto itr1 = (direct1 ? ring1.end() : ring1.begin());
-    auto itr2 = (direct2 ? ring2.end() : ring2.begin());
-    //ring1.insert(itr, ring.begin().m_itr->key, ring.begin().m_itr->info);
-
-    for (int k = 0; k < start_pos; k++) {
-        itr++;
-    }
-
-    for (int i = 0; i < repetitions; i++) {
-        for (int j = 0; j < len1; j++) {
-            ring1.insert(itr1, itr.getPtr()->key, itr.getPtr()->info);
-            itr1 = (direct1 ? ring1.end() : ring1.begin());
-            itr++;
-        }
-        for (int k = 0; k < len2; k++) {
-            ring2.insert(itr2, itr.getPtr()->prev->key, itr.getPtr()->prev->info);
-            itr2 = (direct2 ? ring2.end() : ring2.begin());
-            itr++;
-        }
-    }
-    cout << endl << "******" << endl << "split_by_pos (start_pos=" << start_pos << "): " << endl;
-    cout << endl << "ring1: " << endl << ring1;
-    cout << endl << "ring2: " << endl << ring2;
-}
-
-template<typename Key, typename Info>
-void split_by_key (const Ring <Key, Info>& ring, const Key& start_key, int start_occurrence, bool direct, int repetitions, 
-Ring <Key, Info>& ring1, bool direct1, int len1, 
-Ring <Key, Info>& ring2, bool direct2, int len2)
+int main()
 {
-    auto itr = ring.begin();
-    auto itr1 = (direct1 ? ring1.end() : ring1.begin());
-    auto itr2 = (direct2 ? ring2.end() : ring2.begin());
-    //ring1.insert(itr, ring.begin().m_itr->key, ring.begin().m_itr->info);
+    Ring<int, string> ring;     // int Key, string Info
 
-    for (int k = 0; k < start_occurrence; k++) {
-        if (itr.getPtr()->key == start_key)
-            break;
-        itr++;
-    }
+    cout << "**Empty ring:**" << endl;
+    cout << ring << endl;               // Print empty ring
 
-    for (int i = 0; i < repetitions; i++) {
-        for (int j = 0; j < len1; j++) {
-            ring1.insert(itr1, itr.getPtr()->key, itr.getPtr()->info);
-            itr1 = (direct1 ? ring1.end() : ring1.begin());
-            itr++;
-        }
-        for (int k = 0; k < len2; k++) {
-            ring2.insert(itr2, itr.getPtr()->key, itr.getPtr()->info);
-            itr2 = (direct2 ? ring2.end() : ring2.begin());
-            itr++;
-        }
-    }
-    cout << endl << "******" << endl << "split_by_key (key=" << start_key << "): " << endl;
-    cout << endl << "ring1: " << endl << ring1;
-    cout << endl << "ring2: " << endl << ring2;
+/*--------------------------------------------------*/
+
+    // Node insertion
+    cout << "**Node insertion:**" << endl;
+    ring.insert(ring.end(), 1, "Hello");
+    ring.insert(ring.end(), 5, "world");
+    ring.insert(ring.end(), 7, "heyy");
+    ring.insert(ring.end(), 9, "Lorem ipsum");
+
+    cout << ring << endl;
+
+/*--------------------------------------------------*/
+
+/*--------------------------------------------------*/
+
+    // Node removal
+    cout << "**Node removal with key '7'**" << endl;
+    ring.remove(7, "heyy");     // Remove with key and info
+    cout << ring << endl;
+
+/*--------------------------------------------------*/
+
+/*--------------------------------------------------*/
+
+    // Insert node at a specific location
+    auto itr = ring.begin();                // Point iterator to the beginning of the ring
+    
+    cout << "**Insert a node after the first element**" << endl;
+    itr = ring.insert(itr, 2, "lab");       // Insert it after the first element
+    cout << ring << endl;
+
+    itr++;                                  // Points to 2
+
+    cout << "**Insert a node after the second element**" << endl;
+    ring.insert(itr, 3, "eads");            // Insert it after the second element
+    cout << ring << endl;
+
+/*--------------------------------------------------*/
+
+/*--------------------------------------------------*/
+
+    // Node removal with iterator
+    cout << "**Node removal with iterator (last element):**" << endl;
+    itr = ring.end();           // Again, point the iterator to the end of the ring
+    ring.remove(itr);           // Remove with iterator (last element)
+
+    cout << ring << endl;
+
+    // Add a fifth element before the last node
+    ring.insert(--ring.end(), 4, "test");
+
+/*--------------------------------------------------*/
+
+/*--------------------------------------------------*/
+
+    // Ring with string Key and string Info
+
+    Ring<string, string> ring3;
+
+    cout << endl << "**Ring with string Key and string Info:**" << endl;
+    ring3.insert(ring3.end(), "hello", "world!");
+    ring3.insert(ring3.end(), "nO", "PrOBLEm");
+    ring3.insert(ring3.end(), "134", "this is a string");
+    cout << ring3 << endl;
+
+    ring3.remove("hello", "WORLD");
+    cout << ring3 << endl;
+
+/*--------------------------------------------------*/
+
+
+/*--------------------------------------------------*/
+
+    cout << endl << "Final ring: " << endl << ring << endl;
+
+    cout << endl << "*** External functions: ***" << endl;
+    Ring<int, string> ring1;
+    Ring<int, string> ring2;
+
+    split_by_pos(ring, 1, true, 4, ring1, true, 2, ring2, false, 1);
+    split_by_key(ring, 3, 1, true, 2, ring1, true, 2, ring2, true, 1);
 }
--- c++/ring/ring.hpp
+++ c++/ring/ring.hpp
@@ -1,8 +1,7 @@
-/* Circular doubly-linked list */
 /* the header file ring.hpp */
+// 02/2023
 
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -18,33 +17,33 @@ class Ring {
             Node* prev;
             Node* next;
         };
-
     Node* start;
+    
+    void removeAll();
 
     public:
-        Ring() : start(nullptr) {};
-        ~Ring();
+        Ring() { start = nullptr; }
+        ~Ring() { removeAll(); }
+
+        void remove(const Key& key, const Info& info);    // void remove(Node*& start, Key key, Info info);
+        bool isEmpty(const Ring& r) const;
 
         class Iterator {
             friend class Ring;
 
-            protected:
-                Iterator(Node* ptr) : m_itr(ptr) {}
-                Node* m_itr = new Node();
+            private:
+                Node* m_itr;
                 Node& operator*() { return *m_itr; }
                 Node& operator*() const { return *m_itr; }
                 Node* operator->() { return m_itr; }
 
-            public:
-                using iterator_category = bidirectional_iterator_tag;
-                using value_type = Node;
-                using difference_type = ptrdiff_t;
-                //using pointer = Node*;
-                //using reference = Node&;
+                const Key& getKey(Node*& n) const { return n->key; }
+                const Info& getInfo(Node*& n) const { return n->info; }
 
+            public:
                 Iterator() : m_itr(nullptr) {}
-                ~Iterator() {}
-                
+                Iterator(Node* ptr) : m_itr(ptr) {}
+                //~Iterator() {}
 
                 Iterator& operator++() {        // prefix
                     m_itr = m_itr->next;
@@ -71,31 +70,25 @@ class Ring {
                     return tmp;
                 }
 
-                bool operator==(const Iterator& a) const {
-                    return (m_itr == a.getConstPtr());
-                }
+                bool operator==(const Iterator& a) const { return (m_itr == a.m_itr); }
 
-                bool operator!=(const Iterator& a) const {
-                    return (m_itr != a.getConstPtr());
-                }
+                bool operator!=(const Iterator& a) const { return (m_itr != a.m_itr); }
 
-                Node* getPtr() const {
-                    return m_itr;
-                }
+                const Key& getKey() { return getKey(m_itr); }
 
-                const Node* getConstPtr() const {
-                    return m_itr;
-                }
+                const Info& getInfo() { return getInfo(m_itr); }
+
+                void removeAll() { removeAll(m_itr); }
         }; // End of inner class Iterator
 
-        Iterator begin() const{
+        Iterator begin() const {
             return Iterator(start);
         }
-        Iterator end() {
+        Iterator end() const {
             return Iterator((start==nullptr ? start : start->prev));
         }
 
-        Iterator insert(Iterator position, const Key& key, const Info& info) {
+        Iterator insert(const Iterator& position, const Key& key, const Info& info) {
             if (position == nullptr) {         // empty ring
                 Node* tmp = new Node();
                 tmp->info = info;
@@ -119,7 +112,7 @@ class Ring {
 
         Iterator remove(Iterator position) {        // Node removal with iterators
             if (!position.m_itr) {                      // empty ring
-                cout << "Cannot remove, the ring is empty!" << endl;
+                cerr << "Cannot remove, the ring is empty!" << endl;
             }
             else if (position.m_itr->prev == position.m_itr->next) {      // There is only one node in the ring
                 Node* toDel = position.m_itr;
@@ -149,32 +142,27 @@ class Ring {
             return position;
         }
 
-        void remove(Key key, Info info);    // void remove(Node*& start, Key key, Info info);
-
-        friend ostream& operator<<(ostream &out, Ring& r) {
+        friend ostream& operator<<(ostream &out, const Ring& r) {
             auto itr = r.begin(), end = r.end();
 
-            if (!itr.getPtr()) {
+            if (!r.isEmpty(r)) {
                 out << "The ring is empty!" << endl;
                 return out;
             }
 
             out << "Traversal in forward direction: " << endl;
             do {
-                auto i = *itr.getPtr();
-                out << i.key << "-" << i.info << " ";
+                out << itr.getKey() << "-" << itr.getInfo() << "  ";
                 itr++;
             } while (itr != end);
 
             if (r.begin() != r.end()) {     // if the ring has more than one node
-                auto i = *itr.getPtr();
-                out << i.key << "-" << i.info << " ";
+                out << itr.getKey() << "-" << itr.getInfo() << "  ";
             } out << endl;
 
             out << "Traversal in reverse direction: " << endl;
             do {
-                auto i = *itr.getPtr();
-                out << i.key << "-" << i.info << " ";
+                out << itr.getKey() << "-" << itr.getInfo() << "  ";
                 itr--;
             } while (itr != end);
             out << endl;
@@ -182,5 +170,173 @@ class Ring {
             return out;
         }
 };
+
+template<typename Key, typename Info>
+bool Ring<Key, Info>::isEmpty(const Ring& r) const {
+    if (!r.start)
+        return false;
+    return true;
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::removeAll() {
+    if (!start) {
+        cerr << "Cannot removeAll, the ring is already empty!" << endl;
+        return;
+    }
+
+    Node* tmp = start;
+    Node* tail = new Node();
+
+    do {
+        tail = tmp;
+        tmp = tmp->next;
+    } while (tmp->next != start);
+
+    tail = tail->next;
+
+    if (start) {                    // is ring empty?
+        Node* temp = tail;
+
+        while (temp != start) {     // delete nodes until reaching the start node
+            temp->next = start->next;
+            delete start;
+            start = temp->next;
+        }
+        if (temp == start) {        // one node left
+            delete start;
+            start = nullptr;
+        }
+    }
+}
+
+template<typename Key, typename Info>
+void Ring<Key, Info>::remove(const Key& key, const Info& info) {  // Node removal by key and info
+    if (!start) {
+        cout << "Cannot remove, the ring is empty!" << endl;
+        return;
+    }
+    Node* tmp = start;
+
+    if (tmp->prev == tmp->next) {      // There is only one node in the ring
+        start = nullptr;
+        delete tmp;
+        return;
+    }
+
+    if (tmp->key == key && tmp->info == info) {     // The ring has more than 1 node and
+        Node* toDel = tmp;                          // the node to be removed is the first node
+        tmp = start->prev;
+        start = start->next;
+        tmp->next = start;
+        start->prev = tmp;
+        delete toDel;
+        return;
+    }
+
+    Node* last = tmp;
+
+    while ((tmp->key != key) || (tmp->info != info)) {
+        if (tmp->next == start) {
+            cout << "Cannot remove, the specified key = '" << key << "' and info = '" << info << "' do not exist!" << endl;
+            return;
+        }
+        last = tmp;
+        tmp = tmp->next;
+    }
+
+    if (tmp->next == start) {       // If it is the last node
+        Node* toDel = tmp;
+        last->next = start;
+        start->prev = last;
+        delete toDel;
+    }
+
+    else {
+        Node* toDel = tmp;
+        tmp->next->prev = last;
+        last->next = tmp->next;
+        delete toDel;
+    }
+}
+
+// For instance split_by_pos
+//
+// ring={1,}{2,}{3,}{4,}{5,}
+// start_pos=1, direct=true, repetitions=4,
+// direct1=true, len1=2,
+// direct2=false, len2=1
+//
+// ring1={2,}{3,}{5,}{1,}{3,}{4,}{1,}{2,}
+// ring2={3,}{5,}{2,}{4,}
+
+template<typename Key, typename Info>
+void split_by_pos (const Ring <Key, Info>& ring, unsigned int start_pos, bool direct, unsigned int repetitions, 
+Ring <Key, Info>& ring1, bool direct1, unsigned int len1, 
+Ring <Key, Info>& ring2, bool direct2, unsigned int len2)
+{   
+    if (start_pos < 1) {
+        cout << "split_by_key failed, enter a valid start_pos value!" << endl;
+        return;
+    }
+
+    auto itr = ring.begin();
+    auto itr1 = (direct1 ? ring1.end() : ring1.begin());
+    auto itr2 = (direct2 ? ring2.end() : ring2.begin());
+    //ring1.insert(itr, ring.begin().m_itr->key, ring.begin().m_itr->info);
+
+    for (int k = 0; k < start_pos; k++) {
+        itr++;
+    }
+
+    for (int i = 0; i < repetitions; i++) {
+        for (int j = 0; j < len1; j++) {
+            ring1.insert(itr1, itr.getKey(), itr.getInfo());
+            itr1 = (direct1 ? ring1.end() : ring1.begin());
+            itr++;
+        }
+        for (int k = 0; k < len2; k++) {
+            ring2.insert(itr2, itr.getKey(), itr.getInfo());
+            itr2 = (direct2 ? ring2.end() : ring2.begin());
+            itr++;
+        }
+    }
+    cout << endl << "******" << endl << "split_by_pos (start_pos=" << start_pos << "): " << endl;
+    cout << endl << "ring1: " << endl << ring1;
+    cout << endl << "ring2: " << endl << ring2;
+}
+
+template<typename Key, typename Info>
+void split_by_key (const Ring <Key, Info>& ring, const Key& start_key, unsigned int start_occurrence, bool direct, unsigned int repetitions, 
+Ring <Key, Info>& ring1, bool direct1, unsigned int len1, 
+Ring <Key, Info>& ring2, bool direct2, unsigned int len2)
+{
+    auto itr = ring.begin();
+    auto itr1 = (direct1 ? ring1.end() : ring1.begin());
+    auto itr2 = (direct2 ? ring2.end() : ring2.begin());
+    //ring1.insert(itr, ring.begin().m_itr->key, ring.begin().m_itr->info);
+
+    for (int k = 0; k < start_occurrence; k++) {
+        if (itr.getKey() == start_key)
+            break;
+        itr++;
+    }
+
+    for (int i = 0; i < repetitions; i++) {
+        for (int j = 0; j < len1; j++) {
+            ring1.insert(itr1, itr.getKey(), itr.getInfo());
+            itr1 = (direct1 ? ring1.end() : ring1.begin());
+            itr++;
+        }
+        for (int k = 0; k < len2; k++) {
+            ring2.insert(itr2, itr.getKey(), itr.getInfo());
+            itr2 = (direct2 ? ring2.end() : ring2.begin());
+            itr++;
+        }
+    }
+    cout << endl << "******" << endl << "split_by_key (key=" << start_key << "): " << endl;
+    cout << endl << "ring1: " << endl << ring1;
+    cout << endl << "ring2: " << endl << ring2;
+}
 
 #endif
